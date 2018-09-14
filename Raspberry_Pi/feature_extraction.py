1347a86f97030a364e6bc6a84f8b7b01fd98db42
LAPTOP-HFGK7OGE\pankaj <pankajbhootra@gmail.com>
2018-09-14 16:29:43 +0800

    Add code to extract segments of data

--- Raspberry_Pi/feature_extraction.py
+++ Raspberry_Pi/feature_extraction.py
@@ -1,6 +1,9 @@
 import numpy as np
 from statsmodels import robust
 import pickle
+from sklearn.model_selection import train_test_split
+
+SAVE_FILEPATH = "dummy_dataset\\RawData_ByMove\\"
 
 # for every segment of data, extract the feature vector
 def extract_feature_vector(X):
@@ -15,14 +18,31 @@ def extract_feature_vector(X):
     X_psd = []
     X_peakF = []
     # return feature vector by appending all vectors above as one d-dimension feature vector
-    return np.append(X_mean, [X_var, X_max, X_min, X_off, X_mad, X_psd, X_peakF])
+    return np.append(X_mean, [X_var, X_max, X_min, X_off, X_mad])
 
 # segment data from the raw data files, return list of tuples (segments, move_class)
 # where every tuple represents raw data for that segment and the move_class for that segment
 def get_all_segments(raw_data, move_class):
-
+    limit = (len(raw_data) // 128 ) * 128
+    segments = []
+    for i in range(0, limit, 64):
+        segment = raw_data[i: (i + 128)]
+        segments.append(segment)
+    return segments
 
 if __name__ == "__main__":
     # Get all segments for every move one by one
     # for every segment for a given move, extract the feature vector
     # in the end, store a list of tuple pairs of (feature_vector, move_class) to pickle file
+    raw_data = pickle.load(open(SAVE_FILEPATH + 'data_by_move.pkl', 'rb'))
+    data = []
+    for move in raw_data:
+        segments = get_all_segments(raw_data[move], move)
+        for segment in segments:
+            X = extract_feature_vector(segment)
+            print(move + " " + str(X))
+            data.append((X, move))
+    X, Y = zip(*data)
+    X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2, random_state=42, shuffle=True, stratify=Y)
+    pickle.dump([X_train, Y_train], open(SAVE_FILEPATH + 'train.pkl', 'wb'))
+    pickle.dump([X_val, Y_val], open(SAVE_FILEPATH + 'test.pkl', 'wb'))
