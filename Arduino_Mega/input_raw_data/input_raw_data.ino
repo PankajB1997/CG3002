382f8ea10b8ac4673f9f69907efa52cbdb731457
junyango <junyangooooo@gmail.com>
2018-10-11 16:32:44 +0800

    calibration

--- Arduino_Mega/input_raw_data/input_raw_data.ino
+++ Arduino_Mega/input_raw_data/input_raw_data.ino
@@ -13,16 +13,16 @@
 #define RL 10000
 #define PowerBufferSize 100
 
-//Offsets are to idle state (standing still)
-#define x1_accelRaw -55.18 //0x53 (RIGHT HAND)
-#define y1_accelRaw -252.07 //0x53 (RIGHT HAND)
-#define z1_accelRaw 41.42 //0x53 (RIGHT HAND)
-#define x2_accelRaw 51.97 //0x1D (LEFT HAND)
-#define y2_accelRaw -256.83 //0x1D (LEFT HAND)
-#define z2_accelRaw 16.73//0x1D (LEFT HAND)
-#define x1_gyroRaw 296.02//0x68 (LEFT HAND)
-#define y1_gyroRaw -74.04//0x68 (LEFT HAND)
-#define z1_gyroRaw 153.84//0x68 (LEFT HAND)
+//  //Offsets for calibration of sensors
+//#define x1_accelRaw 55.96 //0x53 (LEFT HAND)
+//#define y1_accelRaw -258.32 //0x53 (LEFT HAND)
+//#define z1_accelRaw 2.62 //0x53 (LEFT HAND)
+//#define x2_accelRaw -21.58 //0x1D (RIGHT HAND)
+//#define y2_accelRaw -255.49 //0x1D (RIGHT HAND)
+//#define z2_accelRaw -20.97//0x1D (RIGHT HAND)
+//#define x1_gyroRaw 172.12//0x68 (RIGHT HAND)
+//#define y1_gyroRaw -74.04//0x68 (RIGHT HAND)
+//#define z1_gyroRaw 291.69//0x68 (RIGHT HAND)
 
 ADXL345 sensorA = ADXL345(DEVICE_A_ACCEL);
 ADXL345 sensorB = ADXL345(DEVICE_B_ACCEL);
@@ -41,9 +41,6 @@ const float scaleFactorAccel = rangeAccel/1023.0;
 //determining the scale factor for gyroscope
 int rangeGyro = 250-(-250);
 const float scaleFactorGyro = rangeGyro/65535.0;
-
-//16 bit integer values for raw data of accelerometers
-int16_t xa_raw, ya_raw, za_raw, xb_raw, yb_raw, zb_raw;
 
 //16 bit integer values for offset data of accelerometers
 int16_t xa_offset, ya_offset, za_offset, xb_offset, yb_offset, zb_offset;
@@ -154,6 +151,9 @@ void loop()
 }
 
 void getScaledReadings() {
+  //16 bit integer values for raw data of accelerometers
+  int16_t xa_raw, ya_raw, za_raw, xb_raw, yb_raw, zb_raw;
+  
   sensorA.getAcceleration(&xa_raw, &ya_raw, &za_raw);
   xa = (xa_raw + xa_offset)*scaleFactorAccel;
   ya = (ya_raw + ya_offset)*scaleFactorAccel;
@@ -198,20 +198,44 @@ float remapVoltage(int volt) {
  * Purpose of adding 255 is to account for downward default acceleration in Z axis to be 1g
  */
 void calibrateSensors() {
-  //Setting range  of ADXL345
+  //Setting range of ADXL345
   Serial.println("Calibrating sensors...");
   sensorA.setRange(ADXL345_RANGE_2G);
   sensorB.setRange(ADXL345_RANGE_2G);
+
+  //16 bit integer values for raw data of accelerometers
+  int16_t xa_raw, ya_raw, za_raw, xb_raw, yb_raw, zb_raw;
+
+  int16_t avg_xa, avg_ya, avg_za, avg_xb, avg_yb, avg_zb, avg_xg, avg_yg, avg_zg;
+
+  for(int i = 0; i<100; i++) {
+      sensorA.getAcceleration(&xa_raw, &ya_raw, &za_raw);
+      sensorB.getAcceleration(&xb_raw, &yb_raw, &zb_raw);
+      sensorC.getRotation(&xg_raw, &yg_raw, &zg_raw);
+      
+      avg_xa += -xa_raw;
+      avg_ya += -ya_raw;
+      avg_za += -za_raw+255;
+    
+      avg_xb += -xb_raw;
+      avg_yb += -yb_raw;
+      avg_zb += -zb_raw+255;
+          
+      avg_xg += -xg_raw;
+      avg_yg += -yg_raw;
+      avg_zg += -zg_raw;
+  }
   
-  xa_offset = -x1_accelRaw;
-  ya_offset = -y1_accelRaw;
-  za_offset = -z1_accelRaw+255;
+  xa_offset = avg_xa / 100;
+  ya_offset= avg_ya /100;
+  za_offset = (avg_za /100);
 
-  xb_offset = -x2_accelRaw;
-  yb_offset = -y2_accelRaw;
-  zb_offset = -z2_accelRaw+255;
+  xb_offset = avg_xb / 100;
+  yb_offset = avg_yb / 100;
+  zb_offset = (avg_zb / 100);
 
-  xg_offset = -x1_gyroRaw;
-  yg_offset = -y1_gyroRaw;
-  zg_offset = -z1_gyroRaw;
+  xg_offset = avg_xg / 100;
+  yg_offset = avg_yg / 100;
+  zg_offset = avg_zg / 100;
+ 
 }
