508e3a4e0ff5272222f8e352d06454a6fcdffe97
PankajB1997 <pankajbhootra@gmail.com>
2018-10-13 12:48:10 +0800

    Update model files with new data collected

--- Raspberry_Pi/raw_data_categorize_by_move.py
+++ Raspberry_Pi/raw_data_categorize_by_move.py
@@ -11,12 +11,19 @@ for move in moves:
     data_by_move[move] = []
     for dancer in os.listdir(RAW_DATASET_PATH):
         move_data_current_dancer = os.path.join(RAW_DATASET_PATH, dancer, move + '.txt')
+        dancerDataAvailable = False
         if os.path.exists(move_data_current_dancer):
             with open(move_data_current_dancer) as textfile:
                 for line in textfile:
                     values = line.split("\t")
+                    if not len(values) == 9:
+                        continue
+                    values = [ val.strip().replace('\n', '') for val in values ]
                     values = list(map(float, values))
                     data_by_move[move].append(values)
+                    dancerDataAvailable = True
+        if dancerDataAvailable == True:
+            print(move_data_current_dancer)
 
 for move in data_by_move:
     print(move + ": " + str(len(data_by_move[move])))
