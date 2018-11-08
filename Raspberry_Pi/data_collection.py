ebaf42a6ff02739888c6a6587f601578086bbc63
jin-ting <pankajbhootra@gmail.com>
2018-11-08 07:03:52 +0000

    update data

--- Raspberry_Pi/data_collection.py
+++ Raspberry_Pi/data_collection.py
@@ -11,8 +11,8 @@ import pickle
 N = 128
 count = 1
 
-danceMove = "test"
-dancer = "bryan"
+danceMove = "logout"
+dancer = "yanyu"
 SAVEPATH = os.path.join("dataset", "RawData", dancer, danceMove + ".txt")
 
 def readLineCR(port):
